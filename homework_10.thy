954df6abe8bbec98cc0bad54cdf3ab4794365b4a
Martin Rau <martin.rau@tum.de>
2018-12-25 19:06:08 +0100

    simplifiy by moving axis into datastructure

--- homework_10.thy
+++ homework_10.thy
@@ -5,13 +5,13 @@ begin
 
 text \<open>
   I did take "Functional Data Structures" last semester and verified the range query algorithm
-  as final project. Those sections are marked (LINE 130-286) and have already been graded but 
+  as final project. Those sections are marked (LINE 125-242) and have already been graded but 
   are here for completeness only.
 
   The construction of balanced k-d trees and the nearest neighbor algorithm are new.
   There is of course some overlap in the initial definitions but I had to change the tree
-  by moving the data form the nodes to the leafs to make the nearest neighbor algorithm work and
-  adjust the work from last semester accordingly.
+  by moving the data form the nodes to the leafs and storing the axis in the node to make the 
+  nearest neighbor algorithm work and adjust the work from last semester accordingly.
 \<close>
 
 text \<open>
@@ -21,21 +21,20 @@ text \<open>
   The tree could also be build with the nodes holding the points and the leafs being empty, but this
   complicates the code and the proofs.
   Every node divides the space into two parts by splitting along a hyperplane.
-  Consider a node n with associated split s in depth d, counting the number of edges from the root.
-  The splitting hyperplane of this point will be the (d mod k) axis with value s.
-  Subsequently all points in the left subtree must have a value at axis (d mod k) that is less or
-  equal than s and all points in the right subtree must have a value at axis (d mod k) that is
+  Consider a node n with associated splitting axis a with value s.
+  All points in the left subtree must have a value at axis a that is less or
+  equal than s and all points in the right subtree must have a value at axis a that is
   greater that v.
 
   e.g.: Consider a 2-d tree.
 
-   0/x-axis:                          N 7
+   0/x-axis:                          N 0 7
 
-   1/y-axis:                N 3                 N 6
+   1/y-axis:                N 1 3                N 1 6
 
-   0/x-axis:         N 2          N 4      (9,6)     N 8
+   0/x-axis:         N 0 2        N 0 4      (9,6)    N 0 8
 
-                 (2,3) (7, 2)  (4,7) (5,4)       (8,7) (9,9)
+                 (2,3) (7, 2)  (4,7) (5,4)        (8,7) (9,9)
 
   Leaf (4,7) is at its current position because 4 <= 7, 7 > 3 and 4 <= 4.
 
@@ -66,16 +65,12 @@ type_synonym split = real
 
 datatype kdt =
   Leaf point
-| Node kdt split kdt
+| Node axis split kdt kdt
 
 definition dim :: "point \<Rightarrow> nat"  where
   "dim p = length p"
 
-definition incr :: "axis \<Rightarrow> dimension \<Rightarrow> axis" where
-  "incr a d = (a + 1) mod d"
-
 declare dim_def[simp]
-declare incr_def[simp]
 
 
 
@@ -87,45 +82,42 @@ text \<open>
 
 fun set_kdt :: "kdt \<Rightarrow> point set" where
   "set_kdt (Leaf p) = {p}"
-| "set_kdt (Node l _ r) = set_kdt l \<union> set_kdt r"
+| "set_kdt (Node _ _ l r) = set_kdt l \<union> set_kdt r"
 
-fun invar' :: "axis \<Rightarrow> dimension \<Rightarrow> kdt \<Rightarrow> bool" where
-  "invar' _ k (Leaf p) \<longleftrightarrow> k > 0 \<and> dim p = k"
-| "invar' a k (Node l s r) \<longleftrightarrow> (\<forall>p \<in> set_kdt l. p!a \<le> s) \<and> (\<forall>p \<in> set_kdt r. p!a > s) \<and>
-    invar' (incr a k) k l \<and> invar' (incr a k) k r"
+fun invar :: "dimension \<Rightarrow> kdt \<Rightarrow> bool" where
+  "invar k (Leaf p) \<longleftrightarrow> dim p = k"
+| "invar k (Node a s l r) \<longleftrightarrow> (\<forall>p \<in> set_kdt l. p!a \<le> s) \<and> (\<forall>p \<in> set_kdt r. p!a > s) \<and>
+    invar k l \<and> invar k r \<and> a < k"
 
-abbreviation invar where
-  "invar \<equiv> invar' 0"
-
-lemma invar'_l:
-  assumes "invar' a k (Node l s r)"
-  shows "invar' (incr a k) k l"
+lemma invar_l:
+  assumes "invar k (Node a s l r)"
+  shows "invar k l"
   using assms by simp
 
-lemma invar'_r:
-  assumes "invar' a k (Node l s r)"
-  shows "invar' (incr a k) k r"
+lemma invar_r:
+  assumes "invar k (Node a s l r)"
+  shows "invar k r"
   using assms by simp
 
-lemma invar'_dim_gt_0:
-  assumes "invar' a k kdt"
-  shows "k > 0"
-  using assms by (induction kdt arbitrary: a) auto
+lemma invar_axis_lt_k:
+  assumes "invar k (Node a s l r)"
+  shows "a < k"
+  using assms by simp
   
-lemma invar'_dim:
-  assumes "invar' a k kdt"
+lemma invar_dim:
+  assumes "invar k kdt"
   shows "\<forall>p \<in> set_kdt kdt. dim p = k"
-  using assms by (induction kdt arbitrary: a) auto
+  using assms by (induction kdt) auto
 
-lemma invar'_l_le_a: 
-  assumes "invar' a k kdt" "kdt = Node l s r"
+lemma invar_l_le_a: 
+  assumes "invar k (Node a s l r)"
   shows "\<forall>p \<in> set_kdt l. p!a \<le> s"
-  using assms invar'.simps(2) by blast
+  using assms by simp
 
-lemma invar'_r_gt_a: 
-  assumes "invar' a k kdt" "kdt = Node l s r"
+lemma invar_r_gt_a: 
+  assumes "invar k (Node a s l r)"
   shows "\<forall>p \<in> set_kdt r. s < p!a"
-  using assms invar'.simps(2) by blast
+  using assms by simp
 
 
 
@@ -165,18 +157,17 @@ definition is_bounding_box :: "dimension \<Rightarrow> point \<Rightarrow> point
 definition point_in_bounding_box :: "dimension \<Rightarrow> point \<Rightarrow> point \<Rightarrow> point \<Rightarrow> bool" where
   "point_in_bounding_box k p p\<^sub>0 p\<^sub>1 \<longleftrightarrow> (\<forall>i < k. p\<^sub>0!i \<le> p!i \<and> p!i \<le> p\<^sub>1!i)"
 
-fun query_area' :: "axis \<Rightarrow> dimension \<Rightarrow> point \<Rightarrow> point \<Rightarrow> kdt \<Rightarrow> point set" where
-  "query_area' _ k p\<^sub>0 p\<^sub>1 (Leaf p) = (
+fun query_area' :: "dimension \<Rightarrow> point \<Rightarrow> point \<Rightarrow> kdt \<Rightarrow> point set" where
+  "query_area' k p\<^sub>0 p\<^sub>1 (Leaf p) = (
     if point_in_bounding_box k p p\<^sub>0 p\<^sub>1 then {p} else {}
   )"
-| "query_area' a k p\<^sub>0 p\<^sub>1 (Node l s r) = (
-    let a' = incr a k in
+| "query_area' k p\<^sub>0 p\<^sub>1 (Node a s l r) = (
     if s < p\<^sub>0!a then
-      query_area' a' k p\<^sub>0 p\<^sub>1 r
+      query_area' k p\<^sub>0 p\<^sub>1 r
     else if s > p\<^sub>1!a then
-      query_area' a' k p\<^sub>0 p\<^sub>1 l
+      query_area' k p\<^sub>0 p\<^sub>1 l
     else
-      query_area' a' k p\<^sub>0 p\<^sub>1 l \<union> query_area' a' k p\<^sub>0 p\<^sub>1 r
+      query_area' k p\<^sub>0 p\<^sub>1 l \<union> query_area' k p\<^sub>0 p\<^sub>1 r
   )"
 
 
@@ -185,36 +176,14 @@ fun query_area' :: "axis \<Rightarrow> dimension \<Rightarrow> point \<Rightarro
 text \<open>Auxiliary lemmas:\<close>
 
 lemma l_pibb_empty:
-  assumes "invar' a k (Node l s r)" "s < p\<^sub>0!a" "a < k"
+  assumes "invar k (Node a s l r)" "s < p\<^sub>0!a"
   shows "{ p \<in> set_kdt l. point_in_bounding_box k p p\<^sub>0 p\<^sub>1 } = {}"
-  using assms
-proof -
-  have "\<forall>q \<in> set_kdt l. q!a \<le> s"
-    using assms(1) invar'_l_le_a by blast
-  hence "\<forall>p \<in> set_kdt l. p!a < p\<^sub>0!a"
-    using assms(2) by auto
-  hence "\<forall>p \<in> set_kdt l. (\<exists>i < k. p!i < p\<^sub>0!i \<or> p\<^sub>1!i < p!i)"
-    using assms(3) by blast
-  hence "\<forall>p \<in> set_kdt l. \<not>point_in_bounding_box k p p\<^sub>0 p\<^sub>1"
-    using point_in_bounding_box_def by fastforce
-  thus ?thesis by blast
-qed
+  using assms invar_l_le_a point_in_bounding_box_def by fastforce
 
 lemma r_pibb_empty:
-  assumes "invar' a k (Node l s r)" "s > p\<^sub>1!a " "a < k"
+  assumes "invar k (Node a s l r)" "s > p\<^sub>1!a "
   shows "{ p \<in> set_kdt r. point_in_bounding_box k p p\<^sub>0 p\<^sub>1 } = {}"
-  using assms
-proof -
-  have "\<forall>q \<in> set_kdt r. s < q!a"
-    using assms(1) invar'_r_gt_a by blast
-  hence "\<forall>p \<in> set_kdt r. p\<^sub>1!a < p!a"
-    using assms(2) by auto
-  hence "\<forall>p \<in> set_kdt r. (\<exists>i < k. p!i < p\<^sub>0!i \<or> p\<^sub>1!i < p!i)"
-    using assms(3) by blast
-  hence "\<forall>p \<in> set_kdt r. \<not>point_in_bounding_box k p p\<^sub>0 p\<^sub>1"
-   using point_in_bounding_box_def by fastforce
-  thus ?thesis by blast
-qed
+  using assms invar_r_gt_a point_in_bounding_box_def by fastforce
 
 
 
@@ -222,26 +191,10 @@ qed
 text \<open>The simplified main theorem:\<close>
 
 theorem query_area':
-  assumes "invar' a k kdt" "a < k"
-  shows "query_area' a k p\<^sub>0 p\<^sub>1 kdt = { p \<in> set_kdt kdt. point_in_bounding_box k p p\<^sub>0 p\<^sub>1 }"
+  assumes "invar k kdt"
+  shows "query_area' k p\<^sub>0 p\<^sub>1 kdt = { p \<in> set_kdt kdt. point_in_bounding_box k p p\<^sub>0 p\<^sub>1 }"
   using assms l_pibb_empty r_pibb_empty
-  by (induction kdt arbitrary: a) (auto simp add: Let_def)
-
-
-
-
-text \<open>
-  I would like to drop explicitly passing the splitting axis into every function.
-  The corresponding query function and lemmas in shorter form:
-\<close>
-
-definition query_area :: "point \<Rightarrow> point \<Rightarrow> kdt \<Rightarrow> point set" where
-  "query_area p\<^sub>0 p\<^sub>1 kdt = query_area' 0 (dim p\<^sub>0) p\<^sub>0 p\<^sub>1 kdt"
-
-theorem query_area:
-  assumes "invar k kdt" "is_bounding_box k p\<^sub>0 p\<^sub>1"
-  shows "query_area p\<^sub>0 p\<^sub>1 kdt = { p \<in> set_kdt kdt. point_in_bounding_box k p p\<^sub>0 p\<^sub>1 }"
-  using assms invar'_dim_gt_0 is_bounding_box_def query_area' query_area_def by auto
+  by (induction kdt) auto
 
 
 
@@ -263,8 +216,8 @@ fun min_max :: "real * real \<Rightarrow> real * real" where
 definition to_bounding_box :: "point \<Rightarrow> point \<Rightarrow> point * point" where
   "to_bounding_box p\<^sub>0 p\<^sub>1 = (let ivs = map min_max (zip p\<^sub>0 p\<^sub>1) in (map fst ivs, map snd ivs))"
 
-definition query :: "point \<Rightarrow> point \<Rightarrow> kdt \<Rightarrow> point set" where
-  "query q\<^sub>0 q\<^sub>1 kdt = (let (p\<^sub>0, p\<^sub>1) = to_bounding_box q\<^sub>0 q\<^sub>1 in query_area p\<^sub>0 p\<^sub>1 kdt)"
+definition query_area :: "point \<Rightarrow> point \<Rightarrow> kdt \<Rightarrow> point set" where
+  "query_area q\<^sub>0 q\<^sub>1 kdt = (let (p\<^sub>0, p\<^sub>1) = to_bounding_box q\<^sub>0 q\<^sub>1 in query_area' (dim q\<^sub>0) p\<^sub>0 p\<^sub>1 kdt)"
 
 
 
@@ -281,10 +234,10 @@ lemma pibb:
   shows "point_in_bounding_box k p p\<^sub>0 p\<^sub>1 \<longleftrightarrow> (\<forall>i < k. min (q\<^sub>0!i) (q\<^sub>1!i) \<le> p!i \<and> p!i \<le> max (q\<^sub>0!i) (q\<^sub>1!i))"
   using assms by (auto simp add: min_def max_def to_bounding_box_def point_in_bounding_box_def)
 
-theorem query:
+theorem query_area:
   assumes "invar k kdt" "k = dim q\<^sub>0" "k = dim q\<^sub>1"
-  shows "query q\<^sub>0 q\<^sub>1 kdt = { x \<in> set_kdt kdt. \<forall>i < k. min (q\<^sub>0!i) (q\<^sub>1!i) \<le> x!i \<and> x!i \<le> max (q\<^sub>0!i) (q\<^sub>1!i) }"
-  using assms pibb tbbibb query_area by (auto simp add: query_def)
+  shows "query_area q\<^sub>0 q\<^sub>1 kdt = { x \<in> set_kdt kdt. \<forall>i < k. min (q\<^sub>0!i) (q\<^sub>1!i) \<le> x!i \<and> x!i \<le> max (q\<^sub>0!i) (q\<^sub>1!i) }"
+  using assms pibb tbbibb query_area' by (auto simp add: query_area_def)
 
 (* THIS IS FROM LAST SEMESTER END *)
 
@@ -363,26 +316,24 @@ lemma sqed_com:
 
 
 
-text\<open>
-  The nearest neighbor algorithm.
-\<close>
+text\<open>The nearest neighbor algorithm.\<close>
 
-fun nearest_neighbor' :: "axis \<Rightarrow> dimension \<Rightarrow> point \<Rightarrow> kdt \<Rightarrow> point" where
-  "nearest_neighbor' _ _ _ (Leaf p) = p"
-| "nearest_neighbor' a k p (Node l s r) = (
+fun nearest_neighbor :: "dimension \<Rightarrow> point \<Rightarrow> kdt \<Rightarrow> point" where
+  "nearest_neighbor _ _ (Leaf p) = p"
+| "nearest_neighbor k p (Node a s l r) = (
     if p!a \<le> s then
-      let candidate = nearest_neighbor' (incr a k) k p l in
+      let candidate = nearest_neighbor k p l in
       if sqed p candidate \<le> sqed' s (p!a) then
         candidate
       else
-        let candidate' = nearest_neighbor' (incr a k) k p r in
+        let candidate' = nearest_neighbor k p r in
         min_by_sqed candidate candidate' p
     else
-      let candidate = nearest_neighbor' (incr a k) k p r in
+      let candidate = nearest_neighbor k p r in
       if sqed p candidate < sqed' s (p!a) then
         candidate
       else
-        let candidate' = nearest_neighbor' (incr a k) k p l in
+        let candidate' = nearest_neighbor k p l in
         min_by_sqed candidate candidate' p
   )"
 
@@ -391,10 +342,10 @@ fun nearest_neighbor' :: "axis \<Rightarrow> dimension \<Rightarrow> point \<Rig
 
 text \<open>First part of main theorem.\<close>
 
-lemma nearest_neighbor'_in_kdt:
-  assumes "invar' a k kdt" "dim p = k"
-  shows "nearest_neighbor' a k p kdt \<in> set_kdt kdt"
-  using assms by (induction kdt arbitrary: a) (auto simp add: Let_def min_by_sqed_def)
+lemma nearest_neighbor_in_kdt:
+  assumes "invar k kdt" "dim p = k"
+  shows "nearest_neighbor k p kdt \<in> set_kdt kdt"
+  using assms by (induction kdt) (auto simp add: Let_def min_by_sqed_def)
 
 
 
@@ -446,7 +397,7 @@ lemma minimize_sqed:
   by (smt sqed'_ge_0)
 
 lemma cutoff_r:
-  assumes "invar' a k (Node l s r)" "dim p = k" "a < k"
+  assumes "invar k (Node a s l r)" "dim p = k"
   assumes "p!a \<le> s" "sqed p c \<le> sqed' s (p!a)"
   shows "\<forall>q \<in> set_kdt r. sqed p c \<le> sqed p q"
 proof standard
@@ -457,19 +408,19 @@ proof standard
 
   (* The distance between p and q is greater or equal the minimized distance between p and q' *)
   have "sqed p q \<ge> sqed' (p!a) (?q'!a)"
-    using A minimize_sqed assms(1,2,3) invar'_dim invar'_r by blast
+    using A minimize_sqed assms(1,2) invar_axis_lt_k invar_dim invar_r by blast
   (* Hence it is greater or equal to the distance from p to s + the distance from s to q' *)
   hence "sqed p q \<ge> sqed' (p!a) s + sqed' s (q!a)"
-    by (smt A assms(1,2,3,4) dim_def nth_list_update_eq invar'_r_gt_a sqed'_split)
+    by (smt A assms(1,2,3) dim_def nth_list_update_eq invar_axis_lt_k invar_r_gt_a sqed'_split)
   (* Hence it is greater or equal to the distance from p to c since sqed' s (q!a) \<ge> 0 *)
   hence "sqed p q \<ge> sqed p c"
-    by (smt assms(5) sqed'_com sqed'_ge_0)
+    by (smt assms(4) sqed'_com sqed'_ge_0)
 
   thus "sqed p c \<le> sqed p q" by blast
 qed
 
 lemma cutoff_l:
-  assumes "invar' a k (Node l s r)" "dim p = k" "a < k"
+  assumes "invar k (Node a s l r)" "dim p = k"
   assumes "p!a > s" "sqed p c < sqed' s (p!a)"
   shows "\<forall>q \<in> set_kdt l. sqed p c \<le> sqed p q"
 proof standard
@@ -479,11 +430,11 @@ proof standard
   let ?q' = "p[a := (q!a)]"
 
   have "sqed p q \<ge> sqed' (p!a) (?q'!a)"
-    using A minimize_sqed invar'_dim invar'_l assms(1,2,3) by blast
+    using A minimize_sqed invar_dim invar_l invar_axis_lt_k assms(1,2) by blast
   hence "sqed p q \<ge> sqed' (p!a) s + sqed' s (q!a)"
-    by (smt A assms(1,2,3,4) dim_def invar'_l_le_a nth_list_update_eq sqed'_com sqed'_split)
+    by (smt A assms(1,2,3) dim_def invar_axis_lt_k invar_l_le_a nth_list_update_eq sqed'_com sqed'_split)
   hence "sqed p q \<ge> sqed p c"
-    by (smt assms(5) sqed'_com sqed'_ge_0)
+    by (smt assms(4) sqed'_com sqed'_ge_0)
 
   thus "sqed p c \<le> sqed p q" by blast
 qed
@@ -493,22 +444,22 @@ qed
 
 text \<open>The main proof and the final theorem.\<close>
 
-lemma nearest_neighbor'_minimal:
-  assumes "invar' a k kdt" "dim p = k" "a < k"
-  shows "\<forall>q \<in> set_kdt kdt. sqed (nearest_neighbor' a k p kdt) p \<le> sqed q p"
+lemma nearest_neighbor_minimal:
+  assumes "invar k kdt" "dim p = k"
+  shows "\<forall>q \<in> set_kdt kdt. sqed (nearest_neighbor k p kdt) p \<le> sqed q p"
   using assms
-proof (induction kdt arbitrary: a)
+proof (induction kdt)
   case (Leaf p)
   thus ?case by simp                                      
 next
-  case (Node l s r)
-  let ?candidate_l = "nearest_neighbor' (incr a k) k p l"
-  let ?candidate_r = "nearest_neighbor' (incr a k) k p r"
+  case (Node a s l r)
+  let ?candidate_l = "nearest_neighbor k p l"
+  let ?candidate_r = "nearest_neighbor k p r"
 
   have IHL: "\<forall>q \<in> set_kdt l. sqed ?candidate_l p \<le> sqed q p"
-    using Node.IH(1) Node.prems(1,3) assms(2) by auto
+    using Node.IH(1) Node.prems(1) invar_axis_lt_k assms(2) by auto
   have IHR: "\<forall>q \<in> set_kdt r. sqed ?candidate_r p \<le> sqed q p"
-    using Node.IH(2) Node.prems(1,3) assms(2) by auto
+    using Node.IH(2) Node.prems(1) invar_axis_lt_k assms(2) by auto
 
   consider (A) "p!a \<le> s \<and> sqed p ?candidate_l \<le> sqed' s (p!a)"
          | (B) "p!a \<le> s \<and> \<not>sqed p ?candidate_l \<le> sqed' s (p!a)"
@@ -519,10 +470,10 @@ next
   proof cases
     case A
     hence "\<forall>q \<in> set_kdt r. sqed p ?candidate_l \<le> sqed p q"
-      using Node.prems(1,3) assms(2) cutoff_r invar'_l nearest_neighbor'_in_kdt by blast
+      using Node.prems(1) assms(2) cutoff_r invar_axis_lt_k invar_l nearest_neighbor_in_kdt by blast
     thus ?thesis using A Node.prems IHL
       apply (auto) 
-      by (metis assms(2) invar'_dim nearest_neighbor'_in_kdt sqed_com)
+      by (metis assms(2) invar_dim nearest_neighbor_in_kdt sqed_com)
   next
     case B
     thus ?thesis using Node.prems IHL IHR
@@ -530,10 +481,10 @@ next
   next
     case C
     hence "\<forall>q \<in> set_kdt l. sqed p ?candidate_r \<le> sqed p q"
-      using Node.prems(1,3) assms(2) cutoff_l invar'_r nearest_neighbor'_in_kdt by blast
+      using Node.prems(1) assms(2) cutoff_l invar_r invar_axis_lt_k nearest_neighbor_in_kdt by blast
     thus ?thesis using C Node.prems IHR 
       apply (auto) 
-      by (metis (mono_tags, lifting) assms(2) invar'_dim nearest_neighbor'_in_kdt sqed_com)
+      by (metis (mono_tags, lifting) assms(2) invar_dim nearest_neighbor_in_kdt sqed_com)
   next
     case D
     thus ?thesis using Node.prems IHL IHR
@@ -541,36 +492,10 @@ next
   qed
 qed
 
-theorem nearest_neighbor':
-  assumes "invar' a k kdt" "dim p = k" "a < k"
-  shows "(\<forall>q \<in> set_kdt kdt. sqed (nearest_neighbor' a k p kdt) p \<le> sqed q p) \<and> nearest_neighbor' a k p kdt \<in> set_kdt kdt"
-  using assms nearest_neighbor'_in_kdt nearest_neighbor'_minimal by simp
-
-
-
-
-text \<open>
-  I would like to drop explicitly passing the splitting axis into every function.
-  Define abbreviations and start splitting at 0th axis.
-\<close>
-
-definition nearest_neighbor :: "point \<Rightarrow> kdt \<Rightarrow> point" where
-  "nearest_neighbor p = nearest_neighbor' 0 (dim p) p"
-
-lemma nearest_neighbor_in_kdt:
-  assumes "invar k kdt" "dim p = k"
-  shows "nearest_neighbor p kdt \<in> set_kdt kdt"
-  using assms by (simp add: nearest_neighbor'_in_kdt nearest_neighbor_def)
-
-lemma nearest_neighbor_minimal:
-  assumes "invar k kdt" "dim p = k"
-  shows "\<forall>q \<in> set_kdt kdt. sqed (nearest_neighbor p kdt) p \<le> sqed q p"
-  using assms by (simp add: invar'_dim_gt_0 nearest_neighbor'_minimal nearest_neighbor_def)
-
 theorem nearest_neighbor:
   assumes "invar k kdt" "dim p = k"
-  shows "(\<forall>q \<in> set_kdt kdt. sqed (nearest_neighbor p kdt) p \<le> sqed q p) \<and> nearest_neighbor p kdt \<in> set_kdt kdt"
-  using assms nearest_neighbor_in_kdt nearest_neighbor_minimal by simp
+  shows "(\<forall>q \<in> set_kdt kdt. sqed (nearest_neighbor k p kdt) p \<le> sqed q p) \<and> nearest_neighbor k p kdt \<in> set_kdt kdt"
+  using assms nearest_neighbor_in_kdt nearest_neighbor_minimal invar_axis_lt_k by simp
 
 
 
