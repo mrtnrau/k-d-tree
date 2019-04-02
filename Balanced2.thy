9c163b37c02cb3f651db7397dc52b9f6bf21fc12
Martin Rau <martin.rau@tum.de>
2019-04-02 19:57:53 +0200

    improve balanced2 2x

--- Balanced2.thy
+++ Balanced2.thy
@@ -3,6 +3,9 @@ imports
   Complex_Main
 begin
 
+
+
+
 type_synonym point = "real list"
 type_synonym axis = nat
 type_synonym dimension = nat
@@ -39,7 +42,7 @@ fun complete_kdt :: "kdt \<Rightarrow> bool" where
 fun invar :: "dimension \<Rightarrow> kdt \<Rightarrow> bool" where
   "invar d (Leaf p) \<longleftrightarrow> dim p = d"
 | "invar d (Node a disc l r) \<longleftrightarrow> (\<forall>p \<in> set_kdt l. p!a \<le> disc) \<and> (\<forall>p \<in> set_kdt r. disc \<le> p!a) \<and>
-    invar d l \<and> invar d r \<and> a < d"
+    invar d l \<and> invar d r \<and> a < d \<and> set_kdt l \<inter> set_kdt r = {}"
 
 definition sorted_wrt_a :: "axis \<Rightarrow> point list \<Rightarrow> bool" where
   "sorted_wrt_a a ps = sorted_wrt (\<lambda>p q. p!a \<le> q!a) ps"
@@ -64,7 +67,7 @@ fun build' :: "axis \<Rightarrow> dimension \<Rightarrow> point list \<Rightarro
       let l = take n sps in
       let g = drop n sps in
       let a' = (a + 1) mod d in
-      Node a (last l ! a) (build' a' d l) (build' a' d g)
+      Node a (hd g ! a) (build' a' d l) (build' a' d g)
   )"
 
 
@@ -101,6 +104,8 @@ lemma AUX5:
   using assms by (induction xs) auto
 
 
+
+
 lemma build'_set:
   assumes "length ps = 2 ^ k"
   shows "set ps = set_kdt (build' a d ps)"
@@ -130,7 +135,7 @@ proof (induction ps arbitrary: a k rule: length_induct)
     ultimately have CHILDREN: "set ?l = set_kdt (build' ?a' d ?l)" "set ?g = set_kdt (build' ?a' d ?g)"
       using 1 by blast+
 
-    have "build' a d ps = Node a (last ?l ! a) (build' ?a' d ?l) (build' ?a' d ?g)"
+    have "build' a d ps = Node a (hd ?g ! a) (build' ?a' d ?l) (build' ?a' d ?g)"
       using False by (meson build'.elims not_less)
     moreover have "set ps = set ?l \<union> set ?g"
       using False by (simp add: AUX0)
@@ -138,63 +143,56 @@ proof (induction ps arbitrary: a k rule: length_induct)
   qed
 qed
 
-lemma build'_invar_single:
-  "length ps = 1 \<Longrightarrow> \<forall>p \<in> set ps. dim p = d \<Longrightarrow> distinct ps \<Longrightarrow> a < d \<Longrightarrow> invar d (build' a d ps)"
-  apply (auto)
-  by (metis hd_in_set length_0_conv nat.distinct(1))
 
-lemma aux_d:
+
+lemma AUX6:
   "distinct ps \<Longrightarrow> distinct (sort_wrt_a a ps)"
-  apply (induction ps)
-   apply (auto simp add: distinct_insort)
-  done
+  by (induction ps) (auto simp add: distinct_insort)
 
-lemma aux_ss:
+lemma AUX7:
   "sorted_wrt_a a (sort_wrt_a a ps)"
   apply (induction ps)
-   apply (auto)
+  apply (auto)
   using sorted_insort_key sorted_map by fastforce
 
-lemma aux_s:
+lemma AUX8:
   assumes "sorted_wrt_a a ps"
   shows "sorted_wrt_a a (take n ps)"
     and "sorted_wrt_a a (drop n ps)"
 proof -
   obtain xs ys where "ps = xs @ ys \<and> xs = take n ps \<and> ys = drop n ps"
     by fastforce
-  thus "sorted_wrt_a a (take n ps)" 
-   and "sorted_wrt_a a (drop n ps)" using assms sorted_wrt_append
-    by fastforce+
+  thus "sorted_wrt_a a (take n ps)" "sorted_wrt_a a (drop n ps)" 
+    using assms sorted_wrt_append by fastforce+
 qed
 
-lemma aux_sss:
-  "sorted_wrt_a a ps \<Longrightarrow> \<forall>p \<in> set ps. p!a \<le> (last ps)!a"
-  apply (induction ps)
-   apply (auto)
-  done
+lemma AUX9:
+  "sorted_wrt_a a ps \<Longrightarrow> \<forall>p \<in> set ps. (hd ps)!a \<le> p!a"
+  by (induction ps) auto
 
-lemma aux_ssss:
+lemma AUX10:
   assumes "sorted_wrt_a a ps"
-  shows "\<forall>p \<in> set (drop n ps). \<forall>p' \<in> set (take n ps). p'!a \<le> p!a"
-  using assms
-  apply (induction ps)
-   apply (auto)
-  by (metis (no_types, lifting) append_take_drop_id sorted_wrt.simps(2) sorted_wrt_append)
+  shows "\<forall>t \<in> set (take n ps). \<forall>d \<in> set (drop n ps). t!a \<le> d!a"
+proof -
+  obtain ts ds where 1: "ps = ts @ ds \<and> ts = take n ps \<and> ds = drop n ps"
+    by fastforce
+  hence "\<forall>t \<in> set ts. \<forall>d \<in> set ds. t!a \<le> d!a"
+    using sorted_wrt_append assms by (metis sorted_wrt_a_def)
+  thus ?thesis using 1 by metis
+qed
 
-lemma q:
-  "length ps > 0 \<Longrightarrow> n > 0 \<Longrightarrow> last (take n ps) \<in> set ps"
-  apply (induction ps)
-   apply (auto)
-  by (metis in_set_takeD last_in_set list.simps(3) neq0_conv set_ConsD take_eq_Nil)
+lemma AUX11:
+  assumes "sorted_wrt_a a ps" "n < length ps"
+  shows " \<forall>t \<in> set (take n ps). t!a \<le> (hd (drop n ps))!a"
+  using assms AUX10 by simp
 
-lemma aux_sssss:
-  assumes "sorted_wrt_a a ps" "length ps > 0" "n > 0"
-  shows " \<forall>p \<in> set (drop n ps). (last (take n ps))!a \<le> p!a"
-  using assms aux_ssss q
-  by auto
+
+
 
 lemma build'_invar:
-  "length ps = 2 ^ k \<Longrightarrow> \<forall>p \<in> set ps. dim p = d \<Longrightarrow> distinct ps \<Longrightarrow> a < d \<Longrightarrow> invar d (build' a d ps)"
+  assumes "length ps = 2 ^ k" "\<forall>p \<in> set ps. dim p = d" "distinct ps" "a < d"
+  shows "invar d (build' a d ps)"
+  using assms
 proof (induction ps arbitrary: a k rule: length_induct)
   case (1 ps)
 
@@ -202,76 +200,63 @@ proof (induction ps arbitrary: a k rule: length_induct)
   let ?a' = "(a + 1) mod d"
   let ?l = "take (length ?sps div 2) ?sps"
   let ?g = "drop (length ?sps div 2) ?sps"
-  let ?disc = "last ?l ! a"
-
-  have A': "?a' < d"
-    using "1.prems"(4) by auto
-
-  have A: "\<forall>p \<in> set ?l. dim p = d"
-    using "1.prems"(2) in_set_takeD by fastforce
-  have B: "distinct ?l"
-    using "1.prems" distinct_take aux_d by simp
-
-  have L: "length ps > 1 \<longrightarrow> invar d (build' ?a' d ?l)"
-    using 1 aux4 aux5 A B A'
-    by (smt length_sort less_numeral_extra(3) mod_by_1 mod_if one_mod_2_pow_eq power_0 sort_wrt_a_def zero_neq_one)
-
-  have C: "\<forall>p \<in> set ?g. dim p = d"
-    using "1.prems"(2)
-    using in_set_dropD by force
-  have D: "distinct ?g"
-    using aux_d distinct_drop
-    using "1.prems"(3) by blast
-
-  have G: "length ps > 1 \<longrightarrow> invar d (build' ?a' d ?g)"
-    using 1 aux6 aux7 C D A'
-    by (smt length_sort less_numeral_extra(3) mod_by_1 mod_if one_mod_2_pow_eq power_0 sort_wrt_a_def zero_neq_one)
-
-  have Q: "length ps > 1 \<longrightarrow> build' a d ps = Node a (last ?l ! a) (build' ?a' d ?l) (build' ?a' d ?g)"
-     by (meson build'.elims not_less)
-      
-  have "length ps > 1 \<longrightarrow> (\<forall>p \<in> set ?g. ?disc \<le> p!a)"
-    using aux_sssss
-    by (metis Euclidean_Division.div_eq_0_iff One_nat_def aux_ss div_2_gt_zero length_greater_0_conv length_sort list.size(3) pos2 sort_wrt_a_def)
-  hence GT: "length ps > 1 \<longrightarrow> (\<forall>p \<in> set_kdt (build' ?a' d ?g). ?disc \<le> p!a)"
-    by (metis "1.prems"(1) aux7 build'_set length_sort sort_wrt_a_def)
-
-  have LL: "length ps > 1 \<longrightarrow> set ?l = set_kdt (build' ?a' d ?l)"
-    using build'_set
-    by (smt "1.prems"(1) aux5 length_sort less_numeral_extra(3) mod_by_1 mod_if one_mod_2_pow_eq power_0 sort_wrt_a_def zero_neq_one)
-
-  have "sorted_wrt_a a ?l"
-    using aux_s aux_ss by blast
-  hence "length ps > 1 \<longrightarrow> (\<forall>p \<in> set ?l. p!a \<le> ?disc)"
-    using aux_sss by blast
-  hence LT: "length ps > 1 \<longrightarrow> (\<forall>p \<in> set_kdt (build' ?a' d ?l). p!a \<le> ?disc)"
-    using Q LL by blast
-
-  have QQ: "length ps > 1 \<longrightarrow> invar d (Node a (last ?l ! a) (build' ?a' d ?l) (build' ?a' d ?g))"
-    using L G LT GT 1
-    using invar.simps(2) by blast
+  let ?disc = "hd ?g ! a"
 
   show ?case
   proof (cases "length ps \<le> 1")
     case True
-then show ?thesis using build'_invar_single "1.prems"
-  by (metis le_antisym one_le_numeral one_le_power)
-next
-  case False
-  then show ?thesis using Q QQ
-    by (metis not_less)
-qed
+    hence "length (hd ps) = d"
+      using AUX2 "1.prems"(1,2) by (cases ps) auto
+    thus ?thesis using True AUX1 by auto
+  next
+    case False
+
+    have A': "?a' < d"
+      using "1.prems"(4) by auto
+    moreover have "\<forall>p \<in> set ?l. dim p = d" "\<forall>p \<in> set ?g. dim p = d"
+      using "1.prems"(2) in_set_takeD in_set_dropD by force+
+    moreover have "distinct ?l" "distinct ?g"
+      using "1.prems"(3) AUX6 distinct_take distinct_drop by blast+
+    moreover have K: "k > 0"
+      using "1.prems" False gr0I by force
+    moreover have LEN_GL: "length ?l = 2 ^ (k - 1)" "length ?g = 2 ^ (k - 1)"
+      using "1.prems" K AUX4 by fastforce+
+    moreover have "length ?l < length ps" "length ?g < length ps"
+      using "1.prems" False AUX5 by auto
+    ultimately have L: "invar d (build' ?a' d ?l)" and G: "invar d (build' ?a' d ?g)"
+      using 1 by blast+
+
+    have "\<forall>p \<in> set ?g. ?disc \<le> p!a"
+      using AUX7 AUX8 AUX9 by blast
+    hence X: "\<forall>p \<in> set_kdt (build' ?a' d ?g). ?disc \<le> p!a"
+      using LEN_GL build'_set by blast
+
+    have "\<forall>p \<in> set ?l. p!a \<le> ?disc"
+      using AUX7 AUX11[of a ?sps "(length ?sps div 2)"] by fastforce
+    hence Y: "\<forall>p \<in> set_kdt (build' ?a' d ?l). p!a \<le> ?disc"
+      using LEN_GL build'_set by blast
+
+    have "set ?l \<inter> set ?g = {}"
+      by (metis "1.prems"(3) AUX6 append_take_drop_id distinct_append)
+    hence Z: "set_kdt (build' ?a' d ?l) \<inter> set_kdt (build' ?a' d ?g) = {}"
+      using LEN_GL build'_set by blast
+
+    have "build' a d ps = Node a (hd ?g ! a) (build' ?a' d ?l) (build' ?a' d ?g)"
+      using False by (meson build'.elims not_less)
+    thus ?thesis using "1.prems"(4) L G  X Y Z by simp
+  qed
 qed
 
+
 lemma XX:
-  "complete kdt \<Longrightarrow> size_kdt kdt = 2 ^ (height_kdt kdt - 1)"
+  "complete_kdt kdt \<Longrightarrow> size_kdt kdt = 2 ^ (height_kdt kdt - 1)"
   apply (induction kdt)
    apply (auto)
   by (metis One_nat_def add_is_0 height_kdt.elims mult_2 one_neq_zero power_eq_if)
   
 
 lemma X:
-  assumes "complete kdt1" "complete kdt2" "size_kdt kdt1 = size_kdt kdt2"
+  assumes "complete_kdt kdt1" "complete_kdt kdt2" "size_kdt kdt1 = size_kdt kdt2"
   shows "height_kdt kdt1 = height_kdt kdt2"
 proof -
   have "2 ^ (height_kdt kdt1 - 1) = 2 ^ (height_kdt kdt2 - 1)"
